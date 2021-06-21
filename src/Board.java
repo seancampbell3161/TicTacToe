51e58aeddf2582229506a6fc3ea79612d4e5f69b
Sean Campbell <sean.campbell3161@gmail.com>
2021-06-21 12:26:57 -0500

    add win cases, draw case, init board with numbers

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,23 @@
 public class Board {
 
     public String[][] gameBoard = new String[3][3];
+    private int placeholder = 1;
+
+    public void initBoard() {
+        for(int i = 0; i < gameBoard.length; i++) {
+            for(int j = 0; j < gameBoard[i].length; j++) {
+                gameBoard[i][j] = Integer.toString(placeholder++);
+            }
+        }
+    }
 
     public void printBoard() {
-        System.out.println(gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2]);
-        System.out.println("---------");
-        System.out.println(gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2]);
-        System.out.println("---------");
-        System.out.println(gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2]);
+        System.out.println("\t\t" + gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2]);
+        System.out.println("\t\t---------");
+        System.out.println("\t\t" + gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2]);
+        System.out.println("\t\t---------");
+        System.out.println("\t\t" + gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2]);
+        System.out.println();
     }
 
     public void makeMove(Player player, int spaceChosen) {
@@ -16,47 +26,65 @@ public class Board {
 
             case 0:
                 gameBoard[0][0] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 1:
                 gameBoard[0][1] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 2:
                 gameBoard[0][2] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 3:
                 gameBoard[1][0] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 4:
                 gameBoard[1][1] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 5:
                 gameBoard[1][2] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 6:
                 gameBoard[2][0] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 7:
                 gameBoard[2][1] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
 
             case 8:
                 gameBoard[2][2] = player.playerToken;
+                System.out.println();
                 printBoard();
+                System.out.println();
                 break;
         }
     }
--- src/Game.java
+++ src/Game.java
@@ -15,24 +15,35 @@ public class Game {
 
             System.out.println("Welcome to Tic Tac Toe!");
 
-            System.out.println("Enter a character for player 1: ");
+            System.out.print("Enter a character for player 1: ");
             Player player1 = new Player();
             player1.playerToken = scan.next();
 
-            System.out.println("Now enter a character for player 2:");
+            System.out.print("Now enter a character for player 2: ");
             Player player2 = new Player();
             player2.playerToken = scan.next();
 
             Board board = new Board();
+            board.initBoard();
+            System.out.println();
             board.printBoard();
 
+
             while(!isWon || !isDraw) {
+                int numOfTurns = 1;
                 System.out.println("Enter a number for player 1 to play: ");
                 board.makeMove(player1, scan.nextInt());
+                numOfTurns++;
                 checkIfWon(board.gameBoard, player1);
 
+                if(numOfTurns == board.gameBoard.length) {
+                    isDraw = true;
+                }
+
                 if(isWon) {
+                    System.out.println("***************");
                     System.out.println("Player 1 won!");
+                    System.out.println("***************");
                     break;
                 } else if (isDraw) {
                     System.out.println("It is a draw!");
@@ -41,18 +52,23 @@ public class Game {
 
                 System.out.println("Enter a number for player 2 to play: ");
                 board.makeMove(player2, scan.nextInt());
+                numOfTurns++;
                 checkIfWon(board.gameBoard, player2);
 
                 if(isWon) {
+                    System.out.println("***************");
                     System.out.println("Player 2 won!");
+                    System.out.println("***************");
                     break;
                 } else if (isDraw) {
+                    System.out.println("***************");
                     System.out.println("It is a draw!");
+                    System.out.println("***************");
                     break;
                 }
             }
 
-
+            System.out.println();
             System.out.println("Play again? y = yes");
             playAgain = scan.next();
         }
@@ -67,6 +83,12 @@ public class Game {
                 player.playerToken) {
             isWon = true;
         } else if (gameBoard[2][0] == player.playerToken && gameBoard[2][1] == player.playerToken && gameBoard[2][2] ==
+                player.playerToken) {
+            isWon = true;
+        } else if (gameBoard[0][0] == player.playerToken && gameBoard[1][1] == player.playerToken && gameBoard[2][2] ==
+                player.playerToken) {
+            isWon = true;
+        } else if (gameBoard[0][2] == player.playerToken && gameBoard[1][1] == player.playerToken && gameBoard[2][0] ==
                 player.playerToken) {
             isWon = true;
         }
